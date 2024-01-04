360980e59459c5fa87202bf2d3826688adebffcf
Tan Phat <hufitp@gmail.com>
2024-01-04 22:07:39 +0700

    Them anh va procject

---
+++ QuanLyXeMay/dao/DataSourceProvider.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyXeMay.dao
+{
+    class DataSourceProvider
+    {
+        static public string CONNECT_STRING = "Data Source=localhost,2008;Initial Catalog=quanlyxemay_db;User ID=sa;Password=123;";
+    }
+}
